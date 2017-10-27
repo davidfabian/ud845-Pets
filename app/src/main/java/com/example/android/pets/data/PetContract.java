8286e9915fac08442c2bb9bbe1dc3fcb61b00409
davidfabian <fabiand.01@gmail.com>
2017-10-27 13:24:36 +0300

    petprovider createde, petcontract amended

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,12 +9,40 @@ import android.provider.BaseColumns;
  */
 //contract for the database
 public final class PetContract {
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
+
     //dummy constructor
     private PetContract() {
     }
 
+
     //class for pets table
-    public final class PetEntry implements BaseColumns {
+
+    public static final class PetEntry implements BaseColumns {
+
+        /**
+         * The content URI to access the pet data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
         //table name defined
         public static final String TABLE_NAME = "pets";
         //fields in pets table
@@ -27,6 +56,7 @@ public final class PetContract {
         public static final int GENDER_MALE = 1;
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_FEMALE = 2;
+
 
     }
 }
